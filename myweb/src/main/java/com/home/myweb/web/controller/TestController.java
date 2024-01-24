b0776c5b12759757c78fa78b1d2cac29525d4412
nolooker <121871676+nolooker@users.noreply.github.com>
2024-01-24 22:20:14 +0900

    commit

---
+++ myweb/src/main/java/com/home/myweb/web/controller/TestController.java
@@ -0,0 +1,32 @@
+package com.home.myweb.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.home.myweb.web.service.TestService;
+
+@RestController
+public class TestController {
+	
+	@Autowired
+	private TestService testService;
+	
+	@GetMapping("/myweb")
+	public String getTest () {
+		
+		String test = testService.getTest();
+		
+		return "myweb";
+	}
+	
+	@PostMapping("/myweb")
+	public String postTest () {
+		
+		String test = testService.getTest();
+		
+		return "myweb";
+	}
+	
+}
---
+++ myweb/src/main/java/com/home/myweb/web/service/TestService.java
@@ -0,0 +1,13 @@
+package com.home.myweb.web.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+	public String getTest() {
+		
+		return "service Test";
+	}
+
+}
